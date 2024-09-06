163eadecab60afc56264b8622946a583496fd5d3
WellingtonFR <wellingtondfr@gmail.com>
2024-09-05 23:08:16 -0300

    Adiciona função para redirecionamento para a página dashboard

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,9 +1,19 @@
+import { FormEvent } from "react";
 import styles from "./styles.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    navigate("/dashboard");
+  }
+
   return (
     <div className={styles.container}>
-      <form action="" className={styles.form}>
+      <form action="" className={styles.form} onSubmit={handleSubmit}>
         <img src="/login.png" alt="login image" width={100} height={100} />
         <h2 className={styles.header}>LOGIN</h2>
         <input type="text" placeholder="Insira seu e-mail" className={styles.input} />

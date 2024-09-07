b3bb694278f8d7bdc2ea1e04267db786ce2df4fc
WellingtonFR <wellingtondfr@gmail.com>
2024-09-06 23:31:56 -0300

    Adiciona a rota signup

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import Signup from "./Signup";
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }

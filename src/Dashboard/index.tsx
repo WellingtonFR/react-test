c25182cb17ef51bed775a0b771a3e83325ca37ec
WellingtonFR <wellingtondfr@gmail.com>
2024-09-06 23:32:19 -0300

    Altera o visual da página dashboard

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export default function Dashboard() {
   return (
     <div className={styles.container}>
-      <h1>Dashboard</h1>
+      <h1 className={styles.title}>Dashboard</h1>
     </div>
   );
 }
